1f64283b30c1738c573d987d25f453ef4d5beb3b
mcantlin <mcantlin2018@falmouthschools.org>
2017-08-03 19:18:28 -0400

    speed changes

--- days_and_motors/days_and_motors.ino
+++ days_and_motors/days_and_motors.ino
@@ -15,45 +15,45 @@ const int Z_STEP_PIN = 46;
 const int Z_DIR_PIN = 48;
 const int Z_EN_PIN = 62;
 
-AccelStepper stepper1(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN); 
-AccelStepper stepper2(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN); 
+AccelStepper stepper1(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
+AccelStepper stepper2(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN);
 AccelStepper stepper3(AccelStepper::DRIVER, Z_STEP_PIN, Z_DIR_PIN);
 
 
 int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int sDay=1;
-int count=1;
+int sDay = 1;
+int count = 1;
 long yr = 2017;
 long steps;
-String calendar="";
+String calendar = "";
 
 
-LiquidCrystal lcd(8,9,4,5,6,7);
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
 void setup() {
-  lcd.begin(16,2);  
+  lcd.begin(16, 2);
   Serial.begin(9600);
 
   pinMode(X_EN_PIN, OUTPUT);
   pinMode(Y_EN_PIN, OUTPUT);
   pinMode(Z_EN_PIN, OUTPUT);
-  
+
   digitalWrite(X_EN_PIN, LOW);
   digitalWrite(Y_EN_PIN, LOW);
   digitalWrite(Z_EN_PIN, LOW);
 
   stepper1.setMaxSpeed(5000);
   stepper2.setMaxSpeed(7000);
-  stepper3.setMaxSpeed(7000);
-  
-  stepper1.setSpeed(70);// ==> ~5 second moon cycle ==> needs to be slightly faster
-  stepper2.setSpeed(60);// ==> ~62 second year ==> needs to be slightly faster
-  stepper3.setSpeed(3000);// ==> ~0.168 second day
+  stepper3.setMaxSpeed(100000);
+
+  stepper1.setSpeed(100);//year
+  stepper2.setSpeed(100);//moon
+  stepper3.setSpeed(40000);//day
 
 
-  
- 
+
+
 }
 
 void loop() {
@@ -63,15 +63,15 @@ void loop() {
   stepper3.runSpeed();
 
   Serial.println(stepper3.currentPosition());
-  if (stepper3.currentPosition()%300==0) 
-  { 
+  if (stepper3.currentPosition() % 300 == 0)
+  {
 
-     date();
+    date();
   }
-  
 
-  
- 
+
+
+
 }
 
 //jan, march, may, july, august,october, december == 31
@@ -80,60 +80,34 @@ void loop() {
 void date()
 {
 
-   
-     if (sDay<=days[count-1])
-      {
-        String zero="";
-        String zero1="";
-        String spacer = " / ";
-        if (sDay < 10) zero = "0";
-        if (count < 10) zero1 = "0";
-        calendar = (zero +sDay + spacer + zero1 + count + spacer + yr);
 
-        
-        
-       /* lcd.print(sDay);
-        lcd.print(" / ");
-        if (count < 10) lcd.print('0');
-        lcd.print(count);
-        lcd.print(" / ");
-        lcd.print(yr);*/
+  if (sDay <= days[count - 1])
+  {
+    String zero = "";
+    String zero1 = "";
+    String spacer = " / ";
+    if (sDay < 10) zero = "0";
+    if (count < 10) zero1 = "0";
+    calendar = (zero + sDay + spacer + zero1 + count + spacer + yr);
 
-        lcd.setCursor(0,0);
-        lcd.print(calendar);
-        sDay++;
-        // for 11.8 rps needs to break for .085 seconds
-      }
-      if (sDay > days[count-1])
-      {
-        count++;
-        sDay=1;
-      }
-      if (count > 12)
-      {
-        yr++;
-        count=1;
-      }
+    lcd.setCursor(0, 0);
+    lcd.print(calendar);
+    sDay++;
+  }
+  if (sDay > days[count - 1])
+  {
+    count++;
+    sDay = 1;
+  }
+  if (count > 12)
+  {
+    yr++;
+    count = 1;
+  }
 }
 
 
 
 
 
-/*void printDate()
-{
-  lcd.setCursor(0,0);
-  lcd.print(sDay);
-  lcd.print(" / ");
-  lcd.print(count);
-  lcd.print(" / ");
-  lcd.print(yr);
 
-}*/
-
-
-/*
- * 20 steps per day
- * 560 to 620 steps per month
- * 7300 to 7320 steps per year
-  */
